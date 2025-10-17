1863959b6d17d1a4ecda9296d9d99fd1d6e63c9c
storebe <storebe@icloud.com>
2025-10-17 14:18:18 -0400

    No changes

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -28,7 +28,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
         // TODO Task 1: Complete this method based on its provided documentation
         //      and the documentation for the dog.ceo API. You may find it helpful
         //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
+        //      as well as the code for parsing JSON responses. /
         // return statement included so that the starter code can compile and run.
         return new ArrayList<>();
     }
